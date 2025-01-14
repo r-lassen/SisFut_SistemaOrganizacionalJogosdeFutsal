413502f18d881d31e1aadc5796c95f46c493fde5
Rafaela Ranieri Lassen <lassenrafaela@gmail.com>
2025-01-14 16:29:41 -0300

    Cadastro e algumas alterações no login, começando a implementar as transações.

---
+++ Font-end/script.js
@@ -0,0 +1,10 @@
+const logoecadastro = document.querySelector('.logoecadastro');
+
+function cadastroActive() {
+    logoecadastro.classList.toggle('active');
+};
+
+function loginActive() {
+    logoecadastro.classList.toggle('active');
+};
+
